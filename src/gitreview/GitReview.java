4655bab4ea3add26da32fefae48ebe7b1e1558b6
NTD1993 <59976447+NTD1993@users.noreply.github.com>
2020-01-23 15:02:38 -0500

    Update GitReview.java
    From GitHub

--- src/gitreview/GitReview.java
+++ src/gitreview/GitReview.java
@@ -20,7 +20,9 @@ public class GitReview {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("A simple example");
+     
+        System.out.println("Modified from GitHub");
     }
 
 }
